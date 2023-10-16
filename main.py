2eeceb28ae2ef2cb51a522f746d842c9612f5f0f
Aryna Reutska <arina.reutskaya110@gmail.com>
2023-10-16 18:41:09 +0300

    update files

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from normalize import normalize
 
 def handle_media(file_name: Path, target_folder: Path):
     target_folder.mkdir(exist_ok=True, parents=True)
-    file_name.replace(target_folder / normalize(file_name.name))
+    new_file_name = normalize(file_name.stem) + file_name.suffix
+    file_name.replace(target_folder / new_file_name)
 
 def handle_archive(file_name: Path, target_folder: Path):
     target_folder.mkdir(exist_ok=True, parents=True)
